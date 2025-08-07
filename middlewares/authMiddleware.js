b1eb269b69c0f7c6564d39aa330162e9ff7714e1
Reyyan Kucukkara <reyyankucukkara42@gmail.com>
2025-08-07 15:06:17 +0300

    Update authMiddleware.js

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -9,7 +9,7 @@ const authMiddleware = (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { id: decoded.id, role: decoded.role }; // id ve role ekledik
+    req.user = { id: decoded.id, role: decoded.role }; 
     next();
   } catch (err) {
     res.status(401).json({ message: "Invalid token" });
